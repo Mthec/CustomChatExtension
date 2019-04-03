13490d69fe81a3e382e1b9754380b9c30d6b2946
Daniel <daniel4592@hotmail.com>
2019-04-03 22:19:09 +0100

    Initial commit for version that works with globalchat mod.

--- src/main/java/mod/wurmunlimited/discord/CustomChatExtension.java
+++ src/main/java/mod/wurmunlimited/discord/CustomChatExtension.java
@@ -1,49 +1,66 @@
 package mod.wurmunlimited.discord;
 
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
 import com.wurmonline.server.Message;
-import com.wurmonline.server.Server;
+import com.wurmonline.server.Servers;
 import com.wurmonline.server.creatures.Communicator;
 import com.wurmonline.server.players.Player;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.ChannelType;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
+import net.spirangle.wurm.ChatHandler;
+import org.gotti.wurmunlimited.modloader.ReflectionUtil;
+import org.gotti.wurmunlimited.modloader.interfaces.Initable;
 import org.gotti.wurmunlimited.modloader.interfaces.MessagePolicy;
 import org.gotti.wurmunlimited.modloader.interfaces.PlayerMessageListener;
 import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 import org.nyxcode.wurm.discordrelay.DiscordRelay;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-public class CustomChatExtension extends ListenerAdapter implements WurmServerMod, Configurable, PlayerMessageListener {
-    private final BiMap<String, String> names = HashBiMap.create();
+public class CustomChatExtension extends ListenerAdapter implements WurmServerMod, Initable, PlayerMessageListener {
+    private final Map<String, String> names = new HashMap<>();
+
+    public CustomChatExtension() {
+        names.put(discordifyName(ChatHandler.TRADE), ChatHandler.TRADE);
+        names.put(discordifyName(ChatHandler.WORLD), ChatHandler.WORLD);
+    }
 
     @Override
-    public void configure(Properties properties) {
-        String channelNames = properties.getProperty("channel_names");
-        for (String name : channelNames.split(","))
-            names.put(name, discordifyName(name));
+    public void init() {
+        try {
+            JDA jda = ReflectionUtil.getPrivateField(null, DiscordRelay.class.getDeclaredField("jda"));
+            jda.addEventListener(this);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private void sendToCustomChat(String channel, final String message){
-        final Message mess = new Message(null, Message.SAY, channel, message);
         if (message.trim().length() > 1) {
-            Server.getInstance().addMessage(mess);
+            if (channel.equals(ChatHandler.WORLD))
+                ChatHandler.getInstance().sendWorldMessage(null, Message.KINGDOM, channel, message, -1, -1, -1);
+            else if (channel.equals(ChatHandler.TRADE))
+                ChatHandler.getInstance().sendTradeMessage(null, Message.TRADE, channel, message, -1, -1, -1);
         }
     }
 
     @Override
     public MessagePolicy onPlayerMessage(Communicator communicator, String message, String title) {
-        // Skip commands
-        if (message.startsWith("!") || message.startsWith("/") || message.startsWith("#")){
-            return MessagePolicy.PASS;
-        }
+        Player player = communicator.getPlayer();
+        if (!player.isMute()) {
+            // Skip commands
+            if (message.startsWith("!") || message.startsWith("/") || message.startsWith("#")){
+                return MessagePolicy.PASS;
+            }
 
-        if (names.containsKey(title)) {
-            Player player = communicator.getPlayer();
-            DiscordRelay.sendToDiscord(title, "<" + (player != null ? player.getName() : "???") + "> " + message, false);
+            if (player.isDead() || (Servers.localServer.entryServer && !player.isReallyPaying() && !player.mayMute()) ||
+                        communicator.isInvulnerable() || (title.equals(ChatHandler.TRADE) && !player.isTradeChannel())) {
+                return MessagePolicy.PASS;
+            } else if (title.equals(ChatHandler.TRADE) || title.equals(ChatHandler.WORLD)) {
+                DiscordRelay.sendToDiscord(title, "<" + player.getName() + "> " + message, false);
+            }
         }
 
         return MessagePolicy.PASS;
@@ -58,7 +75,7 @@ public class CustomChatExtension extends ListenerAdapter implements WurmServerMo
     public void onMessageReceived(MessageReceivedEvent event) {
         super.onMessageReceived(event);
         if (event.isFromType(ChannelType.TEXT) && !event.getAuthor().isBot()) {
-            String wurmChannelName = names.inverse().get(event.getTextChannel().getName());
+            String wurmChannelName = names.get(event.getTextChannel().getName());
 
             if (wurmChannelName != null) {
                 sendToCustomChat(wurmChannelName, "<@" + event.getMember().getEffectiveName() + "> " + event.getMessage().getContent());
